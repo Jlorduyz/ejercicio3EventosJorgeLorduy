71aae068314cccc81622bf1c83ec9fcdfacc01f4
Jorge <jorgelorduy4@outlook.com>
2024-08-06 21:27:15 -0500

    v1.1

--- js/notas.js
+++ js/notas.js
@@ -18,16 +18,37 @@ let contenedor = document.getElementById("contenedor");
 let tituloAGuargar = document.getElementById("tituloNew");
 let textoAGuardar = document.getElementById("textoNew");
 
+function borrarNota(a) {
+  let index = notas.findIndex((objeto) => objeto.id == a);
+
+  if (index !== -1) {
+    notas.splice(index, 1);
+    crearTarjetas(notas, contenedor);
+  }
+}
+document.getElementById("botonBorrar").addEventListener("click", function () {
+  document.getElementById("tituloNew").value = "";
+  document.getElementById("textoNew").value = "";
+});
+
 function crearTarjetas(a, b) {
   b.innerHTML = "";
+  if (a.length == 0) {
+    b.innerHTML = `<p class="fs-2">NO HAY NOTAS PARA MOSTRAR </p>`;
+  }
   for (let i = 0; i < a.length; i++) {
     let tarjeta = document.createElement("div");
     tarjeta.classList.add("card", "text-bg-dark", "mb-3", "notasSize");
-    tarjeta.innerHTML = `<div class="card-header"></div>
+    tarjeta.innerHTML = `<div class="card-header">
+    <input onClick="marcarRealizada(${a[i].id})" type="checkbox" ${
+      a[i].realizada ? "checked" : ""
+    }>
+    <h5 class="card-title text-center">${a[i].titulo}</h5></div>
   <div class="card-body">
-    <h5 class="card-title text-center">${a[i].titulo}</h5>
-    <p class="card-text text-center">${a[i].texto}</p>
-  </div><button type="button"  id="borrarNota" onclick="borrarNota(${a.id})" class="btn btn-danger">Borrar Nota</button>`;
+<p class="card-text text-center">${a[i].texto}</p>
+  </div><button type="button"  id="borrarNota" onclick="borrarNota(${
+    a[i].id
+  })" class="btn btn-danger">Borrar Nota</button>`;
 
     b.appendChild(tarjeta);
   }
@@ -40,19 +61,33 @@ function crearNotas(a, b) {
       id: idGlobal++,
       titulo: titulo,
       texto: texto,
+      realizada: false,
     };
     notas.push(nuevaNota);
   }
 }
+document
+  .getElementById("barraBusqueda")
+  .addEventListener("keydown", (evento) => {
+    console.log(evento);
+  });
 
-document.getElementById("botonBorrar").addEventListener("click", function () {
-  document.getElementById("tituloNew").value = "";
-  document.getElementById("textoNew").value = "";
-});
+let checkSwitch = document.getElementById("checkRealizadas");
+function checkRealizadas() {
+  let notasFiltradas = checkSwitch.checked
+    ? notas.filter((nota) => nota.realizada)
+    : notas;
+  crearTarjetas(notasFiltradas, contenedor);
+}
 
+function marcarRealizada(a) {
+  let nota = notas.find((objeto) => objeto.id == a);
+  nota.realizada = !nota.realizada;
+  crearTarjetas(notas, contenedor);
+}
 crearTarjetas(notas, contenedor);
 document.getElementById("botonGuardar").addEventListener("click", function () {
-  crearNotas(tituloAGuargar, textoAGuardar, contenedor);
+  crearNotas(tituloAGuargar, textoAGuardar);
   crearTarjetas(notas, contenedor);
   document.getElementById("tituloNew").value = "";
   document.getElementById("textoNew").value = "";
